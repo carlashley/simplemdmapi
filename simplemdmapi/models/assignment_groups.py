d7396463452d1e6ea6e32da2904ea08c08abc464
Carl <9841187+carlashley@users.noreply.github.com>
2022-12-02 09:43:25 +1000

    Update assignment_groups.py

--- simplemdmapi/models/assignment_groups.py
+++ simplemdmapi/models/assignment_groups.py
@@ -1,5 +1,5 @@
+from requests.models import Response
 from ..connector import SimpleMDMConnector
-from typing import Any
 
 
 class AssignmentGroups(SimpleMDMConnector):
@@ -11,49 +11,49 @@ class AssignmentGroups(SimpleMDMConnector):
         self.endpoint = endpoint
         super().__init__()
 
-    def assign_app(self, grp_id: int | str, app_id: int | str, **kwargs) -> Any:
+    def assign_app(self, grp_id: int | str, app_id: int | str, **kwargs) -> Response:
         """Assign an application to an assignment group.
 
         :param grp_id: the id value
         :param app_id: the id value"""
         return self.post(url=f"{grp_id}/apps/{app_id}", **kwargs)  # Return 204 status
 
-    def unassign_app(self, grp_id: int | str, app_id: int | str, **kwargs) -> Any:
+    def unassign_app(self, grp_id: int | str, app_id: int | str, **kwargs) -> Response:
         """Unassign an application from an assignment group.
 
         :param grp_id: the id value
         :param app_id: the id value"""
         return self.delete(url=f"{grp_id}/apps/{app_id}", **kwargs)  # Return 204 status
 
-    def assign_device_group(self, grp_id: int | str, device_grp_id: int | str, **kwargs) -> Any:
+    def assign_device_group(self, grp_id: int | str, device_grp_id: int | str, **kwargs) -> Response:
         """Assign a device group to an assignment group.
 
         :param grp_id: the id value
         :param device_grp_id: the id value"""
         return self.post(url=f"{grp_id}/device_groups/{device_grp_id}", **kwargs)  # Return 204 status
 
-    def unassign_device_group(self, grp_id: int | str, device_grp_id: int | str, **kwargs) -> Any:
+    def unassign_device_group(self, grp_id: int | str, device_grp_id: int | str, **kwargs) -> Response:
         """Unassign a device group from an assignment group.
 
         :param grp_id: the id value
         :param device_grp_id: the id value"""
         return self.delete(url=f"{grp_id}/device_groups/{device_grp_id}", **kwargs)  # Return 204 status
 
-    def assign_device(self, grp_id: int | str, device_id: int | str, **kwargs) -> Any:
+    def assign_device(self, grp_id: int | str, device_id: int | str, **kwargs) -> Response:
         """Assign a device to an assignment group.
 
         :param grp_id: the id value
         :param device_id: the id value"""
         return self.post(url=f"{grp_id}/devices/{device_id}", **kwargs)  # Return 204 status
 
-    def unassign_device(self, grp_id: int | str, device_id: int | str, **kwargs) -> Any:
+    def unassign_device(self, grp_id: int | str, device_id: int | str, **kwargs) -> Response:
         """Unassign a device from an assignment group.
 
         :param grp_id: the id value
         :param device_id: the id value"""
         return self.delete(url=f"{grp_id}/devices/{device_id}", **kwargs)  # Return 204 status
 
-    def create(self, name: str, auto_deploy: bool = True, **kwargs) -> Any:
+    def create(self, name: str, auto_deploy: bool = True, **kwargs) -> Response:
         """Create an assignment group.
 
         :param name: assignment group name
@@ -62,23 +62,23 @@ class AssignmentGroups(SimpleMDMConnector):
         params = self._k2p(self.create, vals=locals(), ignored_locals=["name", "auto_deploy"])
         return self.post(params=params, **kwargs)  # Return created assignment group object
 
-    def delete_group(self, grp_id: int | str, **kwargs) -> Any:
+    def delete_group(self, grp_id: int | str, **kwargs) -> Response:
         """Delete an assignment group.
 
         :param grp_id: the id value"""
         return self.delete(url=f"{grp_id}", **kwargs)  # Return 204 status
 
-    def list_all(self, **kwargs) -> Any:
+    def list_all(self, **kwargs) -> Response:
         """List all assignment groups."""
         return self.paginate(**kwargs)  # Return all assignment group objects
 
-    def retrieve(self, grp_id: int | str, **kwargs) -> Any:
+    def retrieve(self, grp_id: int | str, **kwargs) -> Response:
         """Retrieve one application.
 
         :param grp_id: the id value"""
-        return self.get(url=f"{grp_id}", **kwargs).json()  # Return assignment group object
+        return self.get(url=f"{grp_id}", **kwargs)
 
-    def update(self, grp_id: int | str, name: str, auto_deploy: bool = True, **kwargs) -> Any:
+    def update(self, grp_id: int | str, name: str, auto_deploy: bool = True, **kwargs) -> Response:
         """Update details about an assignment group.
 
         :param grp_id: the id value
@@ -87,13 +87,13 @@ class AssignmentGroups(SimpleMDMConnector):
         params = self._k2p(self.update, vals=locals(), ignored_locals=["grp_id", "name", "auto_deploy"])
         return self.patch(url=f"{grp_id}", params=params, **kwargs)  # Return 204 status
 
-    def push_apps(self, grp_id: int | str, **kwargs) -> Any:
+    def push_apps(self, grp_id: int | str, **kwargs) -> Response:
         """Push applications to an assignment group.
 
         :param grp_id: the id value"""
         return self.post(url=f"{grp_id}/push_apps", **kwargs)  # Return 202 status
 
-    def update_apps(self, grp_id: int | str, **kwargs) -> Any:
+    def update_apps(self, grp_id: int | str, **kwargs) -> Response:
         """Push application updates to an assignment group.
 
         :param grp_id: the id value"""
