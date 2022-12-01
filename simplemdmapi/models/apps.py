cb0add81bb46d9804dfa551f304cd88a10acd4a9
Carl <9841187+carlashley@users.noreply.github.com>
2022-12-02 09:41:55 +1000

    Update apps.py

--- simplemdmapi/models/apps.py
+++ simplemdmapi/models/apps.py
@@ -1,5 +1,6 @@
+from typing import Optional
+from requests.models import Response
 from ..connector import SimpleMDMConnector
-from typing import Any, Optional
 
 
 class Apps(SimpleMDMConnector):
@@ -16,7 +17,7 @@ class Apps(SimpleMDMConnector):
                bundle_id: Optional[str] = None,
                binary: Optional[str] = None,
                name: Optional[str] = None,
-               **kwargs) -> Any:
+               **kwargs) -> Response:
         """Upload/create an app. Can only supply one of 'app_store_id', 'bundle_id', or 'binary'.
 
         The 'name' param can only be used with the 'binary' param.
@@ -27,36 +28,36 @@ class Apps(SimpleMDMConnector):
         :param name: the name to use when uploading a binary"""
         files = {"binary": binary} if binary else dict()
         params = self._k2p(self.create, vals=locals(), ignored_locals=["files"])
-        return self.post(params=params, files=files, upload_key="binary", **kwargs).json()  # Return created app object
+        return self.post(params=params, files=files, upload_key="binary", **kwargs)
 
-    def delete_app(self, app_id: int | str, **kwargs) -> Any:
+    def delete_app(self, app_id: int | str, **kwargs) -> Response:
         """Delete an app.
 
         :param app_id: the id value"""
         return self.delete(url=f"{app_id}", **kwargs)  # Return 204 status
 
-    def list_all(self, **kwargs) -> Any:
+    def list_all(self, **kwargs) -> Response:
         """List all applications."""
         self.paginate(**kwargs)  # Return list of app objects
 
-    def list_installs(self, app_id: int | str, **kwargs) -> Any:
+    def list_installs(self, app_id: int | str, **kwargs) -> Response:
         """List all applications.
 
         :param app_id: the id value"""
-        return self.paginate(url=f"{app_id}/installs", **kwargs)  # Return list of app install objects
+        return self.paginate(url=f"{app_id}/installs", **kwargs)
 
-    def retrieve(self, app_id: int | str, **kwargs) -> Any:
+    def retrieve(self, app_id: int | str, **kwargs) -> Response:
         """Retrieve one application.
 
         :param app_id: the id value"""
-        return self.get(url=f"{app_id}", **kwargs).json()  # Return app object
+        return self.get(url=f"{app_id}", **kwargs)  # Return app object
 
     def update(self,
                app_id: int | str,
                binary: Optional[str] = None,
                deploy_to: Optional[str] = None,
                name: Optional[str] = None,
-               **kwargs) -> Any:
+               **kwargs) -> Response:
         """Update details about an app.
 
         :param app_id: the id value
@@ -65,7 +66,7 @@ class Apps(SimpleMDMConnector):
         files = {"binary": binary} if binary else dict()
         params = self._k2p(self.update, vals=locals(), ignored_locals=["app_id", "files"])
         # Return app update object
-        return self.patch(url=f"{app_id}", params=params, files=files, upload_key="binary", **kwargs).json()
+        return self.patch(url=f"{app_id}", params=params, files=files, upload_key="binary", **kwargs)
 
 
 class ManagedAppConfigs(SimpleMDMConnector):
@@ -77,39 +78,39 @@ class ManagedAppConfigs(SimpleMDMConnector):
         self.endpoint = endpoint
         super().__init__()
 
-    def get_config(self, app_id: int | str, **kwargs) -> Any:
+    def get_config(self, app_id: int | str, **kwargs) -> Response:
         """Retrieve managed application configuration.
 
         :param app_id: the id value"""
-        return self.get(url=f"{app_id}/managed_configs", **kwargs).json()  # Return app object
+        return self.get(url=f"{app_id}/managed_configs", **kwargs)  # Return app object
 
     def create(self,
                app_id: int | str,
                key: str,
                value: str,
                value_type: str,
-               **kwargs) -> Any:
+               **kwargs) -> Response:
         """Create a managed app config.
 
         :param key: the key name.
         :param value: default value the key will have
         :param value_type: the type the value is expected to be"""
         params = self._k2p(self.create, vals=locals(), ignored_locals=["app_id"])
-        return self.post(url=f"{app_id}/managed_configs", params=params, **kwargs).json()  # Return created app object
+        return self.post(url=f"{app_id}/managed_configs", params=params, **kwargs)
 
     def delete_config(self,
                       app_id: int | str,
                       config_id: int | str,
-                      **kwargs) -> Any:
+                      **kwargs) -> Response:
         """Delete a managed config for an app.
 
         :param app_id: the id value
         :param config_id: the id value"""
-        return self.delete(url=f"{app_id}/managed_configs/{config_id}", **kwargs)  # Return 204 status
+        return self.delete(url=f"{app_id}/managed_configs/{config_id}", **kwargs)
 
-    def push_updates(self, app_id: int | str, **kwargs) -> Any:
+    def push_updates(self, app_id: int | str, **kwargs) -> Response:
         """Push a managed config for an app to all devices with that app. Only required for changes
         made to a managed app config via API.
 
         :param app_id: the id value"""
-        return self.post(url=f"{app_id}/managed_configs/push", **kwargs).json()  # Return app update object
+        return self.post(url=f"{app_id}/managed_configs/push", **kwargs)
