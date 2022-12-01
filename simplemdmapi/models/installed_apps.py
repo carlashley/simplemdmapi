2cf42be505856660fe6e0451407ea79d4c56215b
Carl <9841187+carlashley@users.noreply.github.com>
2022-12-02 09:45:30 +1000

    Update installed_apps.py

--- simplemdmapi/models/installed_apps.py
+++ simplemdmapi/models/installed_apps.py
@@ -1,5 +1,5 @@
+from requests.models import Response
 from ..connector import SimpleMDMConnector
-from typing import Any
 
 
 class InstalledApps(SimpleMDMConnector):
@@ -13,14 +13,14 @@ class InstalledApps(SimpleMDMConnector):
         self.endpoint = endpoint
         super().__init__()
 
-    def retrieve(self, app_id: int | str, **kwargs) -> Any:
+    def retrieve(self, app_id: int | str, **kwargs) -> Response:
         """Show details of an installed app.
 
         :param app_id: the id value.
         :param kwargs: specific parameters to provide to the underlying requests function."""
         return self.get(url=f"{app_id}", **kwargs)  # Return an installed app object
 
-    def request_management(self, app_id: int | str, **kwargs) -> Any:
+    def request_management(self, app_id: int | str, **kwargs) -> Response:
         """Request management of an unmanaged app installed on a device.
 
         :param app_id: the id value.
@@ -28,7 +28,7 @@ class InstalledApps(SimpleMDMConnector):
         url = f"{app_id}/request_management"
         return self.post(url=url, **kwargs)  # Return 202 status
 
-    def install_update(self, app_id: int | str, **kwargs) -> Any:
+    def install_update(self, app_id: int | str, **kwargs) -> Response:
         """Install app update for an installed app on devices assigned the app.
 
         :param app_id: the id value.
@@ -36,7 +36,7 @@ class InstalledApps(SimpleMDMConnector):
         url = f"{app_id}/update"
         return self.post(url=url, **kwargs)  # Return 202 status
 
-    def uninstall(self, app_id: int | str, **kwargs) -> Any:
+    def uninstall(self, app_id: int | str, **kwargs) -> Response:
         """Uninstall an installed app.
 
         :param app_id: the id value.
