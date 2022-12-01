9b7d6f40c0b07ebbb8b9fb4f27181c281205d9cd
Carl <9841187+carlashley@users.noreply.github.com>
2022-12-02 09:43:07 +1000

    Revert incorrect update

--- simplemdmapi/models/apps.py
+++ simplemdmapi/models/apps.py
@@ -1,100 +1,116 @@
+from typing import Optional
 from requests.models import Response
 from ..connector import SimpleMDMConnector
 
 
-class AssignmentGroups(SimpleMDMConnector):
-    """Assignment Groups.
+class Apps(SimpleMDMConnector):
+    """Apps.
 
-    SimpleMDM API Documentation: https://simplemdm.com/docs/api/#assignment-groups
+    SimpleMDM API Documentation: https://simplemdm.com/docs/api/#apps
     """
-    def __init__(self, endpoint: str = "assignment_groups") -> None:
+    def __init__(self, endpoint: str = "apps") -> None:
         self.endpoint = endpoint
         super().__init__()
 
-    def assign_app(self, grp_id: int | str, app_id: int | str, **kwargs) -> Response:
-        """Assign an application to an assignment group.
+    def create(self,
+               app_store_id: Optional[str] = None,
+               bundle_id: Optional[str] = None,
+               binary: Optional[str] = None,
+               name: Optional[str] = None,
+               **kwargs) -> Response:
+        """Upload/create an app. Can only supply one of 'app_store_id', 'bundle_id', or 'binary'.
 
-        :param grp_id: the id value
+        The 'name' param can only be used with the 'binary' param.
+
+        :param app_store_id: the Apple Store id value for the app to add
+        :param bundle_id: the bundle id value for the app to add
+        :param binary: file path (as a string) to the app to upload, can only upload '.pkg' files
+        :param name: the name to use when uploading a binary"""
+        files = {"binary": binary} if binary else dict()
+        params = self._k2p(self.create, vals=locals(), ignored_locals=["files"])
+        return self.post(params=params, files=files, upload_key="binary", **kwargs)
+
+    def delete_app(self, app_id: int | str, **kwargs) -> Response:
+        """Delete an app.
+
         :param app_id: the id value"""
-        return self.post(url=f"{grp_id}/apps/{app_id}", **kwargs)  # Return 204 status
-
-    def unassign_app(self, grp_id: int | str, app_id: int | str, **kwargs) -> Response:
-        """Unassign an application from an assignment group.
-
-        :param grp_id: the id value
-        :param app_id: the id value"""
-        return self.delete(url=f"{grp_id}/apps/{app_id}", **kwargs)  # Return 204 status
-
-    def assign_device_group(self, grp_id: int | str, device_grp_id: int | str, **kwargs) -> Response:
-        """Assign a device group to an assignment group.
-
-        :param grp_id: the id value
-        :param device_grp_id: the id value"""
-        return self.post(url=f"{grp_id}/device_groups/{device_grp_id}", **kwargs)  # Return 204 status
-
-    def unassign_device_group(self, grp_id: int | str, device_grp_id: int | str, **kwargs) -> Response:
-        """Unassign a device group from an assignment group.
-
-        :param grp_id: the id value
-        :param device_grp_id: the id value"""
-        return self.delete(url=f"{grp_id}/device_groups/{device_grp_id}", **kwargs)  # Return 204 status
-
-    def assign_device(self, grp_id: int | str, device_id: int | str, **kwargs) -> Response:
-        """Assign a device to an assignment group.
-
-        :param grp_id: the id value
-        :param device_id: the id value"""
-        return self.post(url=f"{grp_id}/devices/{device_id}", **kwargs)  # Return 204 status
-
-    def unassign_device(self, grp_id: int | str, device_id: int | str, **kwargs) -> Response:
-        """Unassign a device from an assignment group.
-
-        :param grp_id: the id value
-        :param device_id: the id value"""
-        return self.delete(url=f"{grp_id}/devices/{device_id}", **kwargs)  # Return 204 status
-
-    def create(self, name: str, auto_deploy: bool = True, **kwargs) -> Response:
-        """Create an assignment group.
-
-        :param name: assignment group name
-        :param auto_deploy: whether apps should be automatically pushed when devices join
-                            this assignment group, default is True."""
-        params = self._k2p(self.create, vals=locals(), ignored_locals=["name", "auto_deploy"])
-        return self.post(params=params, **kwargs)  # Return created assignment group object
-
-    def delete_group(self, grp_id: int | str, **kwargs) -> Response:
-        """Delete an assignment group.
-
-        :param grp_id: the id value"""
-        return self.delete(url=f"{grp_id}", **kwargs)  # Return 204 status
+        return self.delete(url=f"{app_id}", **kwargs)  # Return 204 status
 
     def list_all(self, **kwargs) -> Response:
-        """List all assignment groups."""
-        return self.paginate(**kwargs)
+        """List all applications."""
+        self.paginate(**kwargs)  # Return list of app objects
 
-    def retrieve(self, grp_id: int | str, **kwargs) -> Response:
+    def list_installs(self, app_id: int | str, **kwargs) -> Response:
+        """List all applications.
+
+        :param app_id: the id value"""
+        return self.paginate(url=f"{app_id}/installs", **kwargs)
+
+    def retrieve(self, app_id: int | str, **kwargs) -> Response:
         """Retrieve one application.
 
-        :param grp_id: the id value"""
-        return self.get(url=f"{grp_id}", **kwargs)
+        :param app_id: the id value"""
+        return self.get(url=f"{app_id}", **kwargs)  # Return app object
 
-    def update(self, grp_id: int | str, name: str, auto_deploy: bool = True, **kwargs) -> Response:
-        """Update details about an assignment group.
+    def update(self,
+               app_id: int | str,
+               binary: Optional[str] = None,
+               deploy_to: Optional[str] = None,
+               name: Optional[str] = None,
+               **kwargs) -> Response:
+        """Update details about an app.
 
-        :param grp_id: the id value
-        :param auto_deploy: whether apps should be automatically pushed when devices join
-                            this assignment group, default is True."""
-        params = self._k2p(self.update, vals=locals(), ignored_locals=["grp_id", "name", "auto_deploy"])
-        return self.patch(url=f"{grp_id}", params=params, **kwargs)  # Return 204 status
+        :param app_id: the id value
+        :param binary: file path (as a string) to the app to upload, can only upload '.pkg' files
+        :param name: the name to use when uploading a binary"""
+        files = {"binary": binary} if binary else dict()
+        params = self._k2p(self.update, vals=locals(), ignored_locals=["app_id", "files"])
+        # Return app update object
+        return self.patch(url=f"{app_id}", params=params, files=files, upload_key="binary", **kwargs)
 
-    def push_apps(self, grp_id: int | str, **kwargs) -> Response:
-        """Push applications to an assignment group.
 
-        :param grp_id: the id value"""
-        return self.post(url=f"{grp_id}/push_apps", **kwargs)  # Return 202 status
+class ManagedAppConfigs(SimpleMDMConnector):
+    """Managed App Configs.
 
-    def update_apps(self, grp_id: int | str, **kwargs) -> Response:
-        """Push application updates to an assignment group.
+    SimpleMDM API Documentation: https://simplemdm.com/docs/api/#managed-app-configs
+    """
+    def __init__(self, endpoint: str = "apps") -> None:
+        self.endpoint = endpoint
+        super().__init__()
 
-        :param grp_id: the id value"""
-        return self.post(url=f"{grp_id}/update_apps", **kwargs)  # Return 202 status
+    def get_config(self, app_id: int | str, **kwargs) -> Response:
+        """Retrieve managed application configuration.
+
+        :param app_id: the id value"""
+        return self.get(url=f"{app_id}/managed_configs", **kwargs)  # Return app object
+
+    def create(self,
+               app_id: int | str,
+               key: str,
+               value: str,
+               value_type: str,
+               **kwargs) -> Response:
+        """Create a managed app config.
+
+        :param key: the key name.
+        :param value: default value the key will have
+        :param value_type: the type the value is expected to be"""
+        params = self._k2p(self.create, vals=locals(), ignored_locals=["app_id"])
+        return self.post(url=f"{app_id}/managed_configs", params=params, **kwargs)
+
+    def delete_config(self,
+                      app_id: int | str,
+                      config_id: int | str,
+                      **kwargs) -> Response:
+        """Delete a managed config for an app.
+
+        :param app_id: the id value
+        :param config_id: the id value"""
+        return self.delete(url=f"{app_id}/managed_configs/{config_id}", **kwargs)
+
+    def push_updates(self, app_id: int | str, **kwargs) -> Response:
+        """Push a managed config for an app to all devices with that app. Only required for changes
+        made to a managed app config via API.
+
+        :param app_id: the id value"""
+        return self.post(url=f"{app_id}/managed_configs/push", **kwargs)
