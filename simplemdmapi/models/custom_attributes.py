fb825cd8500cad8357122fa3f907d6cc57126377
Carl <9841187+carlashley@users.noreply.github.com>
2022-12-02 09:43:47 +1000

    Update custom_attributes.py

--- simplemdmapi/models/custom_attributes.py
+++ simplemdmapi/models/custom_attributes.py
@@ -1,5 +1,6 @@
+from typing import Optional
+from requests.models import Response
 from ..connector import SimpleMDMConnector
-from typing import Any, Optional
 
 
 class CustomAttributes(SimpleMDMConnector):
@@ -11,34 +12,34 @@ class CustomAttributes(SimpleMDMConnector):
         self.endpoint = endpoint
         super().__init__()
 
-    def create(self, name: str, default_value: Optional[str] = None, **kwargs) -> Any:
+    def create(self, name: str, default_value: Optional[str] = None, **kwargs) -> Response:
         """Create an custom attribute.
 
         :param name: name of the custom attribute
         :param default_value: the default value of the custom attribute"""
         params = self._k2p(self.create, vals=locals(), ignored_locals=["name", "default_value"])
-        return self.post(params=params, **kwargs).json()  # Return custom attribute object
+        return self.post(params=params, **kwargs)
 
-    def delete_attribute(self, attr_id: int | str, **kwargs) -> Any:
+    def delete_attribute(self, attr_id: int | str, **kwargs) -> Response:
         """Delete an custom attribute.
 
         :param attr_id: the id value"""
-        return self.delete(url=f"{attr_id}", **kwargs).json()  # Return ??
+        return self.delete(url=f"{attr_id}", **kwargs)
 
-    def list_all(self, **kwargs) -> Any:
+    def list_all(self, **kwargs) -> Response:
         """List all custom attributes."""
-        return self.paginate(**kwargs)  # Return list of custom attribute objects
+        return self.paginate(**kwargs)
 
-    def retrieve(self, attr_id: int | str, **kwargs) -> Any:
+    def retrieve(self, attr_id: int | str, **kwargs) -> Response:
         """Retrieve one application.
 
         :param attr_id: the id value"""
-        return self.get(url=f"{attr_id}", **kwargs).json()  # Return custom attribute object
+        return self.get(url=f"{attr_id}", **kwargs)  # Return custom attribute object
 
-    def update(self, attr_id: int | str, default_value: Optional[str] = None, **kwargs) -> Any:
+    def update(self, attr_id: int | str, default_value: Optional[str] = None, **kwargs) -> Response:
         """Update details about an custom attribute.
 
         :param attr_id: the id value
         :param default_value: the updated default value"""
         params = self._k2p(self.update, vals=locals(), ignored_locals=["attr_id", "default_value"])
-        return self.patch(url=f"{attr_id}", params=params, **kwargs).json()  # Return ??
+        return self.patch(url=f"{attr_id}", params=params, **kwargs)
