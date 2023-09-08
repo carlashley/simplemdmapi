deec5bef3f428604002f85bbeda0e4a0284fd92f
carlashley <git@macapakr.com>
2023-09-08 18:37:47 +1000

    Rewrite begins

--- simplemdmapi/__init__.py
+++ simplemdmapi/__init__.py
@@ -1,28 +1,7 @@
-from .endpoints.account import Account
-from .endpoints.apps import Apps, ManagedAppConfigs
-from .endpoints.assignment_groups import AssignmentGroups
-from .endpoints.custom_attributes import CustomAttributes
-from .endpoints.device_groups import DeviceGroups
-from .endpoints.devices import ManagedDevices
-from .endpoints.enrollments import Enrollments
-from .endpoints.installed_apps import InstalledApps
-from .endpoints.logs import Logs
-from .endpoints.profiles import CustomConfigProfiles, Profiles
-from .endpoints.servers import DEPServers, PushCertificates
+"""This is a Python implementation of the SimpleMDM API:
+    https://api.simplemdm.com/#introduction
+"""
 
-account = Account()
-apps = Apps()
-managed_app_configs = ManagedAppConfigs()
-assignment_groups = AssignmentGroups()
-custom_attributes = CustomAttributes()
-device_groups = DeviceGroups()
-devices = ManagedDevices()
-enrollments = Enrollments()
-installed_apps = InstalledApps()
-logs = Logs()
-custom_profiles = CustomConfigProfiles()
-profiles = Profiles()
-dep_servers = DEPServers()
-push_certifcatess = PushCertificates()
-
-VERSION = "0.0.20221202"
+__author__ = "Carl Ashley"
+__name__ = "simplemdmapi"
+__version__ = "1.0.20230908"
