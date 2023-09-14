363453b361301871edef189925c100356327768f
carlashley <git@macapakr.com>
2023-09-14 17:09:48 +1000

    streamlined decorators and validations of kwargs/params

--- simplemdmapi/_decorators.py
+++ simplemdmapi/_decorators.py
@@ -1,11 +1,12 @@
 from functools import wraps
-from itertools import combinations
 from requests.adapters import HTTPAdapter, Retry
 from requests.models import Response
 from typing import Callable, Optional
 
 from ._utilities import urljoin
+from ._validators import _any_params_check, _incompatible_params_check, _required_params_check, _validate_param_options
 
+# used for tracking the kwargs that get passed to 'requests.Session.request'
 _requests_kwargs: list[str] = [
     "method",
     "url",
@@ -49,23 +50,6 @@ def _consume_request_kwargs(**kwargs) -> tuple[dict, dict]:
     return (rqst_kwargs, func_kwargs)
 
 
-def _consume_param_kwargs(param_keys: list[str], **kwargs) -> tuple[dict, dict]:
-    """Consumes kwargs that are to be used as params, returns a tuple with
-    the params kwarg dict in the first index position and the remaining kwargs in the
-    second index position.
-    :param params: a list of strings representing the key names of params
-    :param **kwargs: kwargs to consume"""
-    params, remnants = {}, {}
-
-    for k, v in kwargs.items():
-        if k in param_keys:
-            params[k] = v
-        else:
-            remnants[k] = v
-
-    return (params, remnants)
-
-
 def _consume_status_kwargs(self, **kwargs) -> tuple[list[int], list[int]]:
     """Parse a function's keyword arguments for any ignorable/retry status code lists.
     :param **kwargs: kwargs from a function"""
@@ -73,21 +57,6 @@ def _consume_status_kwargs(self, **kwargs) -> tuple[list[int], list[int]]:
     retry = [*kwargs.get("retry_statuses", []), *self.HTTP_RETRY_STATUS_LIST]
 
     return (ignore, retry)
-
-
-def _generate_bad_combinations(params: tuple) -> list[str]:
-    """Generate all possible bad combinations of paramters.
-    :param params: tuple of list of strings or list of strings representing the name of each parameter that
-                   can't be combined"""
-    result = []
-
-    for p in params:
-        _min, _max = len(p) - 1, len(p)
-        cmbns = [*[[c for c in combinations(p, n)] for n in range(_min, _max)][0]]
-        result.extend(cmbns)
-
-    if result:
-        return result
 
 
 def method_params(fn) -> Callable:
@@ -98,84 +67,47 @@ def method_params(fn) -> Callable:
     @wraps(fn)
     def wrap_actions(self, *args, **kwargs) -> Callable:
         """Wrapper to perform actions on the wrapped fucntion from 'wrap_function'."""
-        _class = self.__class__.__name__
+        print(f"method_params args: {args}, kwargs: {kwargs}")
         _fnctn = fn.__name__
-        param_conf = self._method_kwargs.get(_class, {}).get(_fnctn, {})
-        all_params = param_conf.get("all_params", [])
-        any_params = param_conf.get("any_params", [])
-        req_params = param_conf.get("req_params", [])
-        inc_params = param_conf.get("inc_params", [])
-        val_params = param_conf.get("validate", {})
+        param_conf = self._method_kwargs.get(_fnctn, {})
+
+        # all_params = param_conf.get("all_params")
+        any_params = param_conf.get("any_params")
+        req_params = param_conf.get("req_params")
+        inc_params = param_conf.get("inc_params")
+        val_params = param_conf.get("validate")
         file_param = param_conf.get("file_param")
-        bad_combos = _generate_bad_combinations(inc_params)
 
         # Check all required method parameters are provided
         if req_params:
-            for req_param in req_params:
-                if req_param not in kwargs:
-                    raise TypeError(f"{_fnctn}() missing required keyword-only parameter: {req_param!r}")
+            _required_params_check(kwargs, req_params, _fnctn)
 
         # Check any optional parameters where at least one optional parameter is required
         if any_params:
-            if not any(any_param in kwargs for any_param in any_params):
-                raise TypeError(f"{_fnctn}() missing at least one optional keyword-only parameter: {any_params}")
+            _any_params_check(kwargs, any_params, _fnctn)
 
         # Check any incompatible parameter combinations
         if inc_params:
-            for kwarg in kwargs:
-                for combo in bad_combos:
-                    for bad_kwarg in combo:
-                        if bad_kwarg in kwargs and kwarg in inc_params and not bad_kwarg == kwarg:
-                            raise AttributeError(f"{_fnctn}() {bad_kwarg!r} not permitted with {kwarg!r}")
+            _incompatible_params_check(kwargs, inc_params, _fnctn)
 
         # Validate any parameter values
         if val_params:
-            for param, values in val_params.items():
-                value = kwargs.get(param)
+            _validate_param_options(kwargs, val_params, _fnctn)
 
-                if value and value not in values:
-                    err = f"{_fnctn}() unexpected value {value!r} for {param!r}, expecting one of: {values}"
-                    raise ValueError(err)
+        kwargs, func_kwargs = _consume_request_kwargs(**kwargs)
+        file = func_kwargs.get(file_param)
 
-        files = {file_param: kwargs.get(file_param)}
-        request_params = {
-            k: v for k, v in kwargs.copy().items() if k in all_params and file_param and not k == file_param
-        }
-        request_params["files"] = files
-        kwargs = {k: v for k, v in kwargs.copy().items() if k not in all_params}
-        kwargs["params"] = request_params
+        if file:
+            kwargs["files"] = {file_param: file}
+            del func_kwargs[file_param]
+
+        # left over function kwargs get put into the 'params' key as other
+        # decorators/methods rely on this existing
+        kwargs["params"] = func_kwargs
 
         return fn(self, *args, **kwargs)
 
     return wrap_actions
-
-
-# def param_kwargs(params: list[str]) -> Callable:
-#     """Decorator for internal methods that have parameters to be passed as params to the underlying request.
-#     :param params: list of strings representing the kwarg keys that are params."""
-#
-#     def wrap_function(fn: Callable) -> Callable:
-#         """Wraps the method that has the params to parse.
-#         :param fn: the callable being decorated"""
-#
-#         @wraps(fn)
-#         def wrap_actions(self, *args, **kwargs) -> Callable:
-#             """Wrapper to perform actions on the wrapped fucntion from 'wrap_function'."""
-#             print(f"param_kwargs decorator: pre: {kwargs}")
-#             rqst_params, kwargs = _consume_param_kwargs(params, **kwargs)
-#             kwargs["params"] = rqst_params
-#             print(f"param_kwargs decorator: post: {kwargs}")
-#
-#             # raise type error for unexpected kwargs
-#             for k, _ in kwargs.items():
-#                 if k not in [*params, *_requests_kwargs, "ignore_statuses", "retry_statuses"]:
-#                     raise TypeError(f"{fn.__name__}() got an unexpected keyword argument {k!r}")
-#
-#             return fn(self, *args, **kwargs)
-#
-#         return wrap_actions
-#
-#     return wrap_function
 
 
 def file_upload(fn: Callable) -> Callable:
@@ -189,19 +121,13 @@ def file_upload(fn: Callable) -> Callable:
         # passed, for example, the apps.create method can optionally upload a file to SimpleMDM,
         # so test for the existence of an upload file fieldname, and return a post response
         # either way
-        _class = self.__class__.__name__
-        _fnctn = fn.__name__
-        param_conf = self._method_kwargs.get(_class, {}).get(_fnctn, {})
-        file_fn = param_conf.get("file_param")
-        print(f"file_upload pre processing args, kwargs: {args}, {kwargs}")
+        file_fn = self._method_kwargs.get(fn.__name__, {}).get("file_param")
 
-        if file_fn in kwargs.get("params", {}):
-            filename = kwargs["params"].get(file_fn)
-
-            del kwargs["params"][file_fn]  # not needed in params anymore
+        if file_fn in kwargs.get("files", {}):
+            filename = kwargs["files"].get(file_fn)
 
             with open(filename, "rb") as f:
-                kwargs["files"] = {file_fn: f}
+                kwargs["files"][file_fn] = f
                 response = self.post(*args, **kwargs)
                 return response
         else:
@@ -221,13 +147,7 @@ def request(method: str) -> Callable:
         @wraps(fn)  # keep docstrings, and arguments from original function
         def wrap_actions(self, *args, **kwargs) -> Response:
             """Wrapper to perform actions on the wrapped function from 'wrap_function'."""
-            print(f"request dry_run: {self.dry_run}")
-            print(f"request kwargs (start): {kwargs}")
-            # pre-process arguments, consuming args and kwargs
-            print(f"request {kwargs}")
-            rqst_kwargs, func_kwargs = _consume_request_kwargs(**kwargs)
-            ignore, retry = _consume_status_kwargs(self, **func_kwargs)
-            print(f"consumed rqst_kwargs: {rqst_kwargs}, func_kwargs, {func_kwargs}")
+            ignore, retry = _consume_status_kwargs(self, **kwargs)
 
             if args:
                 url_path, rqst_args = _consume_url(*args)
@@ -235,10 +155,14 @@ def request(method: str) -> Callable:
                 url_path, rqst_args = None, []
 
             url = urljoin(self.api_vers, self.endpoint, url_path, base_url=self.BASE_URL)
-            print(f"urljoined: {url}, rqst_args: {rqst_args}")
 
             # modify the session with some values
-            rqst_kwargs.setdefault("timeout", (self.HTTP_CONNECT_TIMEOUT, self.HTTP_READ_TIMEOUT))
+            kwargs.setdefault("timeout", (self.HTTP_CONNECT_TIMEOUT, self.HTTP_READ_TIMEOUT))
+
+            # modify the request headers to post JSON
+            if "json" in kwargs:
+                kwargs.setdefault("headers", {"Content-Type": "application/json"})
+
             self.session.mount(
                 "https://",
                 HTTPAdapter(
@@ -248,19 +172,17 @@ def request(method: str) -> Callable:
                 ),
             )
 
-            if self.dry_run and not method.lower() == "get":
-                print(f"perform {method!r} on {url!r} with args {rqst_args} and kwargs {rqst_kwargs}")
+            if not self.dry_run:
+                response = self.session.request(method, url, *rqst_args, **kwargs)
+
+                # # handle any exceptions that should raise a HTTP status exception, except when it's
+                # # ok to safely ignore those errors
+                if response.status_code not in ignore:
+                    response.raise_for_status()
+
+                return response  # return the object
             else:
-                return None
-            # # now perform the request
-            # response = self.session.request(method, url, *rqst_args, **rqst_kwargs)
-
-            # # handle any exceptions that should raise a HTTP status exception, except when it's
-            # # ok to safely ignore those errors
-            # if response.status_code not in ignore:
-            #     response.raise_for_status()
-
-            # return response  # return the object
+                print(f"perform {method!r} on {url!r} with args {rqst_args} and kwargs {kwargs}")
 
         return wrap_actions
 
@@ -274,31 +196,33 @@ def paginate(fn: Callable) -> Callable:
     @wraps(fn)
     def wrapper_for_paginating(self, *args, **kwargs):
         """Performs the pagination."""
-        print(f"paginate args, kwargs: {args}, {kwargs}")
-        has_more = True
-        existing_params = kwargs.get("params", {})
-        kwargs, params = _consume_request_kwargs(**kwargs)  # don't use consume_param_kwargs, keep actual params intact
-        params.setdefault("limit", 100)
-        params.setdefault("starting_after", 0)
-        params.update(existing_params)
-        kwargs["params"] = params
+        if not self.dry_run:
+            if not kwargs.get("params"):
+                kwargs["params"] = {}
 
-        while has_more:
-            response = self.get(*args, **kwargs)
-            json_data = response.json()
-            objects = json_data.get("data", [])
+            has_more = True
+            kwargs["params"].setdefault("limit", self.HTTP_PAGINATE_MAX_RESULTS)
+            kwargs["params"].setdefault("starting_after", 0)
 
-            if objects:
-                for obj in objects:
-                    yield obj
+            while has_more:
+                response = self.get(*args, **kwargs)
+                json_data = response.json()
+                objects = json_data.get("data", [])
 
-                kwargs["params"]["starting_after"] = objects[-1].get("id")
-            has_more = json_data.get("has_more", False)
+                if objects:
+                    for obj in objects:
+                        yield obj
+
+                    kwargs["params"]["starting_after"] = objects[-1].get("id")
+                has_more = json_data.get("has_more", False)
+        else:
+            # do some action in dry run mode so something is returned
+            self.get(*args, **kwargs)
 
     return wrapper_for_paginating
 
 
-def url_suffixes(suffix: str, add_kwarg_vals_to_url: Optional[list] = []):
+def url_suffixes(suffix: str, add_param_vals_to_url: Optional[list] = []):
     """Decorator for adding an additional suffix to the base url and endpoint, along with
     and additional values from kwargs provided to the API method, for example, adding 'bluetooth'
     to 'https://a.simplemdm.com/api/v1/devices/{id}' to become:
@@ -315,29 +239,30 @@ def url_suffixes(suffix: str, add_kwarg_vals_to_url: Optional[list] = []):
         @wraps(fn)
         def wrap_action(self, *args, **kwargs) -> Callable:
             """This wraps the method and returns the function with a new set of *args"""
-            print(fn.__name__)
-            # a list of values from kwargs["params"] that need to get added to the URL
-            if kwargs.get("params"):
-                url_kwarg_param_vals = [v for k, v in kwargs["params"].items() if k in add_kwarg_vals_to_url]
-                # update params to reflect consumed values that don't get posted as a param because they form
-                # part of the url
-                kwargs["params"] = {k: v for k, v in kwargs["params"].items() if k not in add_kwarg_vals_to_url}
+            params = kwargs.get("params", {})
+
+            if params and add_param_vals_to_url:
+                param_paths = [v for k, v in params.items() if k in add_param_vals_to_url]
+
+                # remove the params from 'add_param_vals_to_url' as these are not passed as values in 'params'
+                # because they'll form part of the url
+                kwargs["params"] = {k: v for k, v in params.items() if k not in add_param_vals_to_url}
             else:
-                url_kwarg_param_vals = []
+                param_paths = None
 
             try:
                 new_path = f"{args[0]}/{suffix}"
             except IndexError:
-                # this may have unintended consequences as it is untested if this is the appropriate action to take
-                raise Exception(f"Error: could not append {suffix!r} to URL")
+                # fall back to just inserting the suffix; warning, this might not be the most appropriate path forward
+                new_path = suffix
             finally:
-                if add_kwarg_vals_to_url:
-                    values_path = "/".join(kwargs.get(v) for v in url_kwarg_param_vals)
-                    new_path = f"{new_path}/{values_path}"
+                if param_paths:
+                    param_paths = "/".join(param_paths)
+                    new_path = f"{new_path}/{param_paths}"
 
                 args = [new_path, *args[1:]] if args else [new_path]
-                print(f"url_suffixes args, kwargs: {args}, {kwargs}")
-                return fn(self, *args, **kwargs)
+
+            return fn(self, *args, **kwargs)
 
         return wrap_action
 
--- simplemdmapi/_validators.py
+++ simplemdmapi/_validators.py
@@ -1,5 +1,67 @@
 from functools import wraps
+from itertools import combinations
 from typing import Callable
+
+
+def _generate_bad_combinations(params: tuple) -> list[str]:
+    """Generate all possible bad combinations of paramters.
+    :param params: tuple of list of strings or list of strings representing the name of each parameter that
+                   can't be combined"""
+    result = []
+
+    for p in params:
+        _min, _max = len(p) - 1, len(p)
+        cmbns = [*[[c for c in combinations(p, n)] for n in range(_min, _max)][0]]
+        result.extend(cmbns)
+
+    if result:
+        return result
+
+
+def _required_params_check(kwargs: dict, req_params: list, fn_name: str) -> None:
+    """Check kwargs for required parameters.
+    :param kwargs: dictionary object of kwargs
+    :param req_params: list of required params
+    :param fn_name: function name for exceptions"""
+    for req_param in req_params:
+        if req_param not in kwargs:
+            raise TypeError(f"{fn_name}() missing required keyword-only parameter: {req_param!r}")
+
+
+def _any_params_check(kwargs: dict, any_params: list, fn_name) -> None:
+    """Check kwargs for any paramters that are optional but where one or more must be provided.
+    :param kwargs: dictionary object of kwargs
+    :param any_params: list of any optional 'required' params
+    :param fn_name: function name for exceptions"""
+    if not any(any_param in kwargs for any_param in any_params):
+        raise TypeError(f"{fn_name}() missing at least one optional keyword-only parameter: {any_params}")
+
+
+def _incompatible_params_check(kwargs: dict, inc_params: list, fn_name) -> None:
+    """Check kwargs for any paramters that are incompatible with other parameters.
+    :param kwargs: dictionary object of kwargs
+    :param inc_params: list of any incompatible parameters
+    :param fn_name: function name for exceptions"""
+    bad_combos = _generate_bad_combinations(inc_params)
+
+    for kwarg in kwargs:
+        for combo in bad_combos:
+            for bad_kwarg in combo:
+                if bad_kwarg in kwargs and kwarg in inc_params and not bad_kwarg == kwarg:
+                    raise AttributeError(f"{fn_name}() {bad_kwarg!r} not permitted with {kwarg!r}")
+
+
+def _validate_param_options(kwargs: dict, val_params: dict, fn_name) -> None:
+    """Validate values for parameters that only accept specific values.
+    :param kwargs: dictionary object of kwargs
+    :param val_params: dict of any value parameters to check, {"param": ["valid", "values"]}
+    :param fn_name: function name for exceptions"""
+    for param, values in val_params.items():
+        value = kwargs.get(param)
+
+        if value and value not in values:
+            err = f"{fn_name}() unexpected value {value!r} for {param!r}, expecting one of: {values}"
+            raise ValueError(err)
 
 
 def validate_pin(param: str, length: int):
@@ -14,11 +76,15 @@ def validate_pin(param: str, length: int):
             :params *args: positional arguments that are paramters of the decorated function
             :params **kwargs: positional keyword arguments that are parameters of the decorated
                               function"""
-            if not len(kwargs.get(param)) == length:
-                raise ValueError(f"{fn.__name__}() option for {param!r} does not meet length requirement of {length}")
+            _param = kwargs.get("params", {}).get(param)
 
-            if not all(n.isdigit() for n in kwargs.get(param)):
-                raise ValueError(f"{fn.__name__}() option for {param!r} contains non-numeric characters")
+            if _param:
+                if not len(_param) == length:
+                    err = f"{fn.__name__}() option for {param!r} does not meet length requirement of {length}"
+                    raise ValueError(err)
+
+                if not all(n.isdigit() for n in kwargs.get("params", {}).get(param)):
+                    raise ValueError(f"{fn.__name__}() option for {param!r} contains non-numeric characters")
 
         return wrapper_b
 
